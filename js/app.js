6327d446f798cddd9b9659b2e28d79575e91ca7f
Scott Hsu <scotthsumail@yahoo.com>
2017-02-26 19:07:43 -0500

    made some naming changes

--- js/app.js
+++ js/app.js
@@ -1,76 +1,72 @@
 (function() {
     "use strict";
-
-
     $(document).ready(() => {
         const gitOrgModule = function() {
-            const searchField = $('.searchBar');
-            const gitOrgBtn = $('.getUserOrg');
+            const noOrg = document.querySelector('.noGitOrg');
             const apiKey = '01ca0b9658eb146f2ea950a399ae045c2e098570';
 
-            // const userName =;
-
             class GitOrg {
-                constructor(gitObjs) {
-                    // this.image = gitObjs.avatar_url;
-                    this.login = gitObjs.login;
+                constructor(details) {
+                    this.image = details.avatar_url;
+                    this.title = details.login;
                     this.build();
                 }
                 build() {
                     const source = $('#gitOrgTemplate').html();
                     const template = Handlebars.compile(source);
                     const context = {
-                        // image: this.image,
-                        name: this.name
+                        image: this.image,
+                        title: this.title
                     };
                     const html = template(context);
                     $('.org-display').append(html);
                 }
             }
 
-            function bindEvents() {
-                getApiData(value);
-                //
-                //     $('.userSearch').on('submit', function() {
-                //         event.preventDefault();
-                //         console.log('shit');
-                const value = event.target[0].value;
-                //         getApiData(value);
-                //         this.reset();
-                //         return value;
-                //     });
-                //     $(gitOrgBtn).on('click', () => {
-                //         getApiData(value);
-                //         console.log('in');
-                //     });
+            function resetList() {
+                $('.gitOrgObj').remove();
+                $('.org-display div').remove();
             }
 
-            // function showGitOrgBtn() {
-            //     $('.getUserOrg').on('click', function() {
-            //         event.preventDefault();
-            //         console.log('in');
-            //     });
-            // }
+            function bindEvents() {
+                $('.searchBar').on('submit', function() {
+                    event.preventDefault();
+                    let value = event.target[0].value;
+                    getApiData(value);
+                    resetList();
+                    this.reset();
+                    return value;
+                });
+
+                function getOrgResults() {
+                    $('.getUserOrg').on('click', function() {
+                        event.preventDefault();
+                    });
+                }
+            }
+
+
 
             function getApiData(query) {
-                // this medthod encodes
                 query = encodeURIComponent(query);
                 const settings = {
                     'async': true,
                     'crossDomain': true,
                     'method': 'GET',
                     'url': `https://api.github.com/users/${query}/orgs?api_key=${apiKey}`
-
                 };
-
                 $.ajax(settings).then(function(response) {
-                    new GitOrg(response.results);
+                    if (response.length !== 0) {
+                        for (let i = 0; i < response.length; i++) {
+                            new GitOrg(response[i]);
+                        }
 
+                    } else $('.org-display').html('<div class="noGitOrg"> This user is not associated with any organizations </div>');
                 });
+
             }
 
             function init() {
-
                 bindEvents();
             }
             return {
@@ -80,5 +76,6 @@
         const gitOrg = gitOrgModule();
         gitOrg.init();
     });
+
 
 })();
